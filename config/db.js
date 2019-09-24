81bdb1b6b6b8fee520c46b502be320e3a9108761
Jandoso <carol.jandoso@gmail.com>
2019-09-24 15:42:37 -0300

    password

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports ={mongoURI: "mongodb+srv://jandoso:<a1b2c3d4e5>@cluster0-34kgb.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports ={mongoURI: "mongodb+srv://jandoso:a1b2c3d4e5@cluster0-34kgb.mongodb.net/test?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 }
